d4a39c9973fe51562112f7c262f5ba22ba058c2c
junnukim1007gmail.com <junnukim1007@gmail.com>
2026-01-24 12:23:05 +0900

    feat: init project

---
+++ src/main/java/com/junwoo/hamkke/HamkkeApplication.java
@@ -0,0 +1,13 @@
+package com.junwoo.hamkke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamkkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamkkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junwoo/hamkke/HamkkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junwoo.hamkke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamkkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
